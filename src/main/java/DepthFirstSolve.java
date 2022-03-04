e1e24e12ba7d26b1313d256f3a7cf39793202bee
kambed <kamil.bednarek.2001@gmail.com>
2022-03-04 20:57:39 +0100

    depth + breadth algorithm implemented (#5)
    * depth + breadth algorithm implemented
    
    * Iteration number as const, private fields
    
    * Gitignore change for .class
    
    * Small errors fixes from PR (const capital letter, spaces)
    
    
    const capital

---
+++ src/main/java/DepthFirstSolve.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+
+public class DepthFirstSolve {
+    private FifteenPuzzle currentBoard;
+    private Stack<FifteenPuzzle> boardsToCheck = new Stack<>();
+    private final int ITERATIONS = 20;
+
+    public DepthFirstSolve(FifteenPuzzle currentBoard) {
+        this.currentBoard = currentBoard;
+        try {
+            this.solve();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void solve() throws CloneNotSupportedException {
+        System.out.println(currentBoard.toString());
+        if (currentBoard.check()) {
+            return;
+        }
+
+        //move left
+        if (!currentBoard.getLastMove().equals("R") && currentBoard.getEmptyY() != 0
+                && currentBoard.getIterations() < ITERATIONS) {
+            FifteenPuzzle moveL = currentBoard.clone();
+            moveL.moveEmptyL();
+            boardsToCheck.push(moveL);
+        }
+
+        //move right
+        if (!currentBoard.getLastMove().equals("L") && currentBoard.getEmptyY() != 3
+                && currentBoard.getIterations() < ITERATIONS) {
+            FifteenPuzzle moveR = currentBoard.clone();
+            moveR.moveEmptyR();
+            boardsToCheck.push(moveR);
+        }
+
+        //move up
+        if (!currentBoard.getLastMove().equals("D") && currentBoard.getEmptyX() != 0
+                && currentBoard.getIterations() < ITERATIONS) {
+            FifteenPuzzle moveU = currentBoard.clone();
+            moveU.moveEmptyU();
+            boardsToCheck.push(moveU);
+        }
+
+        //move down
+        if (!currentBoard.getLastMove().equals("U") && currentBoard.getEmptyX() != 3
+                && currentBoard.getIterations() < ITERATIONS) {
+            FifteenPuzzle moveD = currentBoard.clone();
+            moveD.moveEmptyD();
+            boardsToCheck.push(moveD);
+        }
+        this.currentBoard = boardsToCheck.pop();
+        if (!this.boardsToCheck.empty()) {
+            this.solve();
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,21 @@ public class Main {
             System.out.println(val);
             return;
         }
+        int[][] start = {
+                {5,3,7,1},
+                {15,13,9,11},
+                {2,8,6,10},
+                {4,0,12,14}
+        };
+        int[][] solved = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,0}
+        };
+        FifteenPuzzle fp = new FifteenPuzzle(start);
+        //new DepthFirstSolve(fp);
+        new BreadthFirstSolve(fp);
+        System.out.println("Finished");
     }
 }
