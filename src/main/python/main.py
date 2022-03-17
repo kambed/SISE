78194b8388b24c6b22d153eeda8f3c1e11b7fc11
RadekBucki <radoslaw.bucki26@gmail.com>
2022-03-17 22:50:33 +0100

    Refactor

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -14,7 +14,7 @@ def generate():
     # 'number_of_processed_states', 'max_recursion_depth', 'duration'
 
     criteria = ['długość znalezionego rozwiązania', 'liczba stanów odwiedzonych', 'liczba stanów przetworzonych',
-                'maksymalna osiągnięta głębokość rekursji', 'czas trwania procesu obliczeniowego']
+                'maksymalna osiągnięta głębokość rekursji', 'czas trwania procesu obliczeniowego [ms]']
 
     strategies = ['bfs', 'dfs', 'astr']
     strategiesTitles = ['BFS', 'DFS', 'A*']
@@ -35,12 +35,12 @@ def generate():
 
     for i in range(0, len(criteria)):
         plt.figure(figsize=(12.8, 9.6))
-
-        plt.subplot(2, 2, 1)
         plt.subplots_adjust(bottom=0.06, left=0.1, right=0.99, top=0.9)
         plt.rcParams.update({'font.size': 12})
-        # resultsTotal[strategy][move]
-        resultsTotal = []
+
+        plt.subplot(2, 2, 1)
+        width = 0.8 / len(strategies)
+        first = x - (width * math.floor(len(strategies) / 2))
         for j in range(0, len(strategies)):
             movesResults = []
             for k in range(0, len(moves)):
@@ -53,20 +53,17 @@ def generate():
                     movesResults.append(0)
                 else:
                     movesResults.append(sum(newArray) / len(newArray))
-            resultsTotal.append(movesResults)
+            plt.bar(first + j * width, movesResults, width, label=strategiesTitles[j])
 
-        width = 0.8 / len(strategies)
-        first = x - (width * math.floor(len(strategies) / 2))
-        for k in range(0, len(strategies)):
-            plt.bar(first + k * width, resultsTotal[k], width, label=strategiesTitles[k])
         plt.ylabel(criteria[i])
         plt.title('Ogółem', fontsize=20)
         plt.xticks(x, moves)
         plt.legend()
 
         plt.subplot(2, 2, 2)
-        # resultsAstr[typeOfStrategy][move]
-        resultsAstr = []
+        width = 0.8 / len(typesOfStrategies[strategies[2]])
+        first = x - (width / 2)
+
         for j in range(0, len(typesOfStrategies[strategies[2]])):
             movesResults = []
             for k in range(0, len(moves)):
@@ -80,19 +77,16 @@ def generate():
                     movesResults.append(0)
                 else:
                     movesResults.append(sum(newArray) / len(newArray))
-            resultsAstr.append(movesResults)
+            plt.bar(first + j * width, movesResults, width, label=typesOfStrategiesTitles[strategies[2]][j])
 
-        width = 0.8 / len(typesOfStrategies[strategies[2]])
-        first = x - (width / 2)
-        for k in range(0, len(typesOfStrategies[strategies[2]])):
-            plt.bar(first + k * width, resultsAstr[k], width, label=typesOfStrategiesTitles[strategies[2]][k])
         plt.title(strategiesTitles[2], fontsize=20)
         plt.xticks(x, moves)
         plt.legend()
 
         plt.subplot(2, 2, 3)
-        # resultsBfs[typeOfStrategy][move]
-        resultsBfs = []
+        width = 0.8 / len(typesOfStrategies[strategies[0]])
+        first = x - (width * math.floor(len(typesOfStrategies[strategies[0]]) / 2))
+
         for j in range(0, len(typesOfStrategies[strategies[0]])):
             movesResults = []
             for k in range(0, len(moves)):
@@ -106,12 +100,8 @@ def generate():
                     movesResults.append(0)
                 else:
                     movesResults.append(sum(newArray) / len(newArray))
-            resultsBfs.append(movesResults)
+            plt.bar(first + j * width, movesResults, width, label=typesOfStrategiesTitles[strategies[0]][j])
 
-        width = 0.8 / len(typesOfStrategies[strategies[0]])
-        first = x - (width * math.floor(len(typesOfStrategies[strategies[0]]) / 2))
-        for k in range(0, len(typesOfStrategies[strategies[0]])):
-            plt.bar(first + k * width, resultsBfs[k], width, label=typesOfStrategiesTitles[strategies[0]][k])
         plt.title(strategiesTitles[0], fontsize=20)
         plt.xticks(x, moves)
         plt.ylabel(criteria[i])
@@ -119,8 +109,9 @@ def generate():
         plt.legend(ncol=2)
 
         plt.subplot(2, 2, 4)
-        # resultsDfs[typeOfStrategy][move]
-        resultsDfs = []
+        width = 0.8 / len(typesOfStrategies[strategies[1]])
+        first = x - (width * math.floor(len(typesOfStrategies[strategies[1]]) / 2))
+
         for j in range(0, len(typesOfStrategies[strategies[1]])):
             movesResults = []
             for k in range(0, len(moves)):
@@ -134,12 +125,8 @@ def generate():
                     movesResults.append(0)
                 else:
                     movesResults.append(sum(newArray) / len(newArray))
-            resultsDfs.append(movesResults)
+            plt.bar(first + j * width, movesResults, width, label=typesOfStrategiesTitles[strategies[1]][j])
 
-        width = 0.8 / len(typesOfStrategies[strategies[1]])
-        first = x - (width * math.floor(len(typesOfStrategies[strategies[1]]) / 2))
-        for k in range(0, len(typesOfStrategies[strategies[1]])):
-            plt.bar(first + k * width, resultsDfs[k], width, label=typesOfStrategiesTitles[strategies[1]][k])
         plt.title(strategiesTitles[1], fontsize=20)
         plt.xticks(x, moves)
         plt.xlabel('Głębokość')
