885549e239bbff58c603f4b33cbfbb37990171fd
Radosław Bucki <radoslaw.bucki26@gmail.com>
2022-04-08 16:04:16 +0200

    Simplify and rename neuron

---
+++ task2/src/main/java/Neuron.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+import java.util.function.Function;
+
+public class Neuron {
+    private final int numberOfInputs;
+    private double[] weights;
+    private final Function<Double, Double> activationFunction;
+    private boolean learningTime = true;
+
+    public Neuron(int numberOfInputs, Function<Double, Double> activationFunction) {
+        this.numberOfInputs = numberOfInputs;
+        this.weights = new double[numberOfInputs];
+        this.activationFunction = activationFunction;
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (new Random()).nextDouble(-1.0, 1.0);
+        }
+    }
+
+    public double[] getWeights() throws IllegalAccessException {
+        if (!learningTime) {
+            throw new IllegalAccessException("This neuron is in working time.");
+        }
+        return weights;
+    }
+
+    public void setWeights(double[] weights) throws IllegalAccessException {
+        if (!learningTime) {
+            throw new IllegalAccessException("This neuron is in working time.");
+        }
+        this.weights = weights;
+    }
+
+    public void setWeight(int index, double weight) throws IllegalAccessException {
+        if (!learningTime) {
+            throw new IllegalAccessException("This neuron is in working time.");
+        }
+        weights[index] = weight;
+    }
+
+    public double getTotalNeuronExcitation(double[] input) {
+        if (input.length != numberOfInputs) {
+            throw new IllegalArgumentException(
+                    "This neuron should get " + numberOfInputs + "inputs, " + input.length + " given."
+            );
+        }
+
+        double totalNeuronExcitation = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            totalNeuronExcitation += input[i] * weights[i];
+        }
+
+        return activationFunction.apply(totalNeuronExcitation);
+    }
+
+    public boolean isLearningTime() {
+        return learningTime;
+    }
+
+    public void switchToLearningTime() {
+        this.learningTime = true;
+    }
+
+    public void switchToWorkingTime() {
+        this.learningTime = false;
+    }
+}
--- task2/src/main/java/neutron/NeutronWorkingTime.java
+++
@@ -1,8 +0,0 @@
-package neutron;
-
-public class NeutronWorkingTime extends Neutron {
-    public NeutronWorkingTime(NeuronLearningTime neutron) {
-        super(neutron.numberOfInputs, neutron.activationFunction);
-        this.weights = neutron.weights;
-    }
-}
