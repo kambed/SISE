d4b3dee34a06cf027dca31264bec79fdbe44c1a6
Radosław Bucki <radoslaw.bucki26@gmail.com>
2022-04-13 09:35:41 +0200

    Create NeuralNetwork

--- task2/src/main/java/backend/Neuron.java
+++ task2/src/main/java/backend/Neuron.java
@@ -1,15 +1,17 @@
+package backend;
+
 import java.util.Random;
 import java.util.function.Function;
 
 public class Neuron {
     private final int numberOfInputs;
-    private double[] weights;
+    private Double[] weights;
     private final Function<Double, Double> activationFunction;
     private boolean learningTime = true;
 
     public Neuron(int numberOfInputs, Function<Double, Double> activationFunction) {
         this.numberOfInputs = numberOfInputs;
-        this.weights = new double[numberOfInputs];
+        this.weights = new Double[numberOfInputs];
         this.activationFunction = activationFunction;
         for (int i = 0; i < weights.length; i++) {
             do {
@@ -18,28 +20,28 @@ public class Neuron {
         }
     }
 
-    public double[] getWeights() throws IllegalAccessException {
+    public Double[] getWeights() throws IllegalAccessException {
         if (!learningTime) {
             throw new IllegalAccessException("This neuron is in working time.");
         }
         return weights;
     }
 
-    public void setWeights(double[] weights) throws IllegalAccessException {
+    public void setWeights(Double[] weights) throws IllegalAccessException {
         if (!learningTime) {
             throw new IllegalAccessException("This neuron is in working time.");
         }
         this.weights = weights;
     }
 
-    public void setWeight(int index, double weight) throws IllegalAccessException {
+    public void setWeight(int index, Double weight) throws IllegalAccessException {
         if (!learningTime) {
             throw new IllegalAccessException("This neuron is in working time.");
         }
         weights[index] = weight;
     }
 
-    public double getTotalNeuronExcitation(double[] input) {
+    public Double getTotalNeuronExcitation(double[] input) {
         if (input.length != numberOfInputs) {
             throw new IllegalArgumentException(
                     "This neuron should get " + numberOfInputs + "inputs, " + input.length + " given."
